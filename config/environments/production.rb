bba67e15413fd82cdb2fc178aa4ca05019e8b60a
masato.matsuda <masato220@gmail.com>
2018-12-29 16:00:24 +0000

    set mailer for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # for Devise
+  config.action_mailer.default_url_options = { host: 'harusarnet.herokuapp.com', port: 80 }
+  
+  
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => ENV['GMAIL_USERNAME'], #gmailアドレス
+  :password => ENV['GMAIL_PASSWORD'], #gmailパスワード
+  :authentication => 'plain',
+}
 end
