bb95a5a5e6a7877b924426be40906338382c658c
masato.matsuda <masato220@gmail.com>
2019-02-21 11:48:17 +0000

    media query, copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     time.strftime("%Y-%m-%d　%H:%M　")
   end 
   
+  def copyright
+    text = "Copyright © 2018"
+    if Date.today.year > 2018
+      text << "-" + Date.today.year.to_s
+    end
+    text << " HarusarNet."
+    text
+  end
 end
